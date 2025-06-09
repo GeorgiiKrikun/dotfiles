9ea3d403b04eb79f1010267cbc69c0012bbf323b
Georgii <georgii.krikun@celantur.com>
2025-06-09 05:41:54 +0200

    feat(deps): add git and zsh setup to bootstrap script
    - Added git installation to the bootstrap script for dependency management.
    - Included commands to install zsh and change the default shell to zsh.
    - Integrated oh-my-zsh installation without prompting for shell usage.
    - Removed redundant bashrc cargo path setup comments.
    - Deleted unused submodule reference for global/nvim.

--- deps/bootstrap.sh
+++ deps/bootstrap.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -ex
 
-apt update && apt install -y curl build-essential sudo
+apt update && apt install -y curl build-essential sudo git
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . $HOME/.cargo/env
 rustup default stable
 cargo install just
-
-# put cargo into bashrc
-#if ! grep -q 'export PATH="$HOME/.cargo/bin:$PATH"' "$HOME/.bashrc"; then
-#    echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> "$HOME/.bashrc"
-#fi
 
 echo ". $HOME/.cargo/env" >> ${HOME}/.bashrc
 
