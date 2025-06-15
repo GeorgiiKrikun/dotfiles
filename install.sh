1e8c1b7b7d80185c03f118049a68812a51106713
Georgii <georgii.krikun@celantur.com>
2025-06-15 10:07:41 +0200

    Take care of the case where the zsh is not installed

--- install.sh
+++ install.sh
@@ -6,12 +6,24 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 bash ${SCRIPT_DIR}/deps/bootstrap.sh 
 JUST=${HOME}/.cargo/bin/just
 
+if ! command -v zsh >/dev/null; then
+  echo "Zsh does not exist, we will install it."
+  ZSH_EXISTS=false
+else
+  echo "Zsh exists, skipping Zsh install."
+  ZSH_EXISTS=true
+fi
 
-cd ${SCRIPT_DIR}/deps
-${JUST} install-zsh
-${JUST} install-oh-my-zsh 
-${JUST} change-shell-zsh
-${JUST} setup-cargo-zsh
+if [ "$ZSH_EXISTS" = true ]; then
+  echo "Zsh exists, skipping Zsh install."
+else
+  echo "Zsh does not exist, we will install it."
+  cd ${SCRIPT_DIR}/deps
+  ${JUST} install-zsh
+  ${JUST} install-oh-my-zsh 
+	${JUST} change-shell-zsh
+  ${JUST} setup-cargo-zsh
+fi
 
 cd ${SCRIPT_DIR}/configs/zsh
 ${JUST} setup
