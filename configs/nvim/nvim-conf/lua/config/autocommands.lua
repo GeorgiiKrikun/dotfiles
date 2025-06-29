c35e6330537fbd76a64974296726ef2110c48a91
Georgii <georgii.krikun@celantur.com>
2025-06-29 09:05:49 +0200

    Some refactoring progress

---
+++ configs/nvim/nvim-conf/lua/config/autocommands.lua
@@ -0,0 +1,61 @@
+vim.api.nvim_create_autocmd("WinEnter", {
+	callback = function()
+		vim.opt_local.number = true
+		vim.opt_local.relativenumber = true
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	callback = function()
+		vim.opt_local.number = true
+		vim.opt_local.relativenumber = true
+	end,
+})
+
+vim.api.nvim_create_autocmd("CmdwinEnter", {
+	callback = function()
+		vim.opt_local.number = false
+		vim.opt_local.relativenumber = false
+	end,
+})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	callback = function()
+		vim.opt_local.number = false
+		vim.opt_local.relativenumber = false
+	end,
+})
+
+-- Load custom commands from a file custom.lua
+vim.api.nvim_create_autocmd("VimEnter", {
+	once = true,
+	callback = function()
+		local cwd = vim.fn.getcwd()
+		local custom_lua = cwd .. "/.nvim/custom.lua"
+		if vim.fn.filereadable(custom_lua) == 1 then
+			vim.notify("Loading custom commands from: " .. custom_lua, vim.log.levels.INFO)
+			dofile(custom_lua)
+		end
+		--require(commands_file_path)
+		--    local file = read_file(commands_file_path)
+		--    if file == nil then
+		--      log_message('File not found: ' .. commands_file_path)
+		--      return
+		--    end
+		--    log_message('File content: ' .. file)
+	end,
+})
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
---
+++ configs/nvim/nvim-conf/lua/config/hotkeys.lua
@@ -0,0 +1,90 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+vim.keymap.set("n", "<leader>H", ":ClangdSwitchSourceHeader<CR>", { desc = "Switch Header/Source" })
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+--close and delete the current buffer
+vim.keymap.set("n", "<C-w>bd", ":bd<CR>", { desc = "Close and delete current buffer" })
+
+-- Delete the selection without yanking it
+vim.keymap.set("n", "D", '"_d', { desc = "Delete without yanking" })
+vim.keymap.set("v", "D", '"_d', { desc = "Delete without yanking (visual mode)" })
+vim.keymap.set("n", "X", '"_x', { desc = "Delete char without yanking" })
+vim.keymap.set("v", "X", '"_x', { desc = "Delete char without yanking (visual mode)" })
+vim.keymap.set("n", "C", '"_c', { desc = "Replace without yanking" })
+vim.keymap.set("v", "C", '"_c', { desc = "Replace without yanking (visual mode)" })
+vim.keymap.set("n", "<leader>ot", ":tabnew | terminal<CR>izsh<CR>", { desc = "Open terminal in new tab" })
+vim.keymap.set("n", "<leader>Lca", ":lua vim.lsp.buf.code_action()<CR>", { desc = "LSP Code [A]ction" })
+
+-- Insert carriage return in normal mode
+vim.keymap.set("n", "<S-CR>", "i<CR><Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-CR>", "a<CR><Esc>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>i", "i <Esc>", { silent = true })
+
+-- Open neogit status window
+vim.keymap.set("n", "<leader>gs", ":Neogit<CR>", { desc = "Open [G]it [S]tatus" })
+
+-- Copilot keymaps
+-- vim.keymap.set('n', '<leader>cc', ':CopilotChat<CR>', { desc = 'Open Copilot [C]hat' })
+vim.keymap.set("n", "<leader>ct", ":CopilotChatToggle<CR>", { desc = "[C]opilot Chat [T]oggle" })
+vim.keymap.set("n", "<leader>cd", ":CopilotChatDocs<CR>", { desc = "[C]opilot Chat [D]ocs" })
+vim.keymap.set("n", "<leader>cc", ":CopilotChatCommit<CR>", { desc = "[C]opilot Chat [C]ommit" })
+vim.keymap.set("n", "<leader>ce", ":CopilotChatExplain<CR>", { desc = "[C]opilot Chat [E]xplain" })
+vim.keymap.set("v", "<leader>ce", ":'<,'>CopilotChatExplain<CR>", { desc = "[C]opilot Chat [E]xplain" })
+
+-- Diagnostic keymaps
+--vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Set diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>Q", vim.cmd.copen, { desc = "Open diagnostic [Q]uickfix window" })
+vim.keymap.set("n", "<leader>C", vim.cmd.cclose, { desc = "Close diagnostic [C]lose window" })
+
+vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+	expr = true,
+	replace_keycodes = false,
+})
+vim.g.copilot_no_tab_map = true
+vim.keymap.set("i", "<C-L>", "<Plug>(copilot-accept-word)")
+-- Evaluate the word or range under the cursor LLDB/GDB
+--
+vim.keymap.set("n", "<leader>de", ":GdbEvalWord<CR>", { desc = "Evaluate word under cursor" })
+vim.keymap.set("v", "<leader>de", ":GdbEvalRange<CR>", { desc = "Evaluate range under cursor" })
+
+vim.keymap.set("n", "<leader>Ct", ":tabclose<CR>", { desc = "[C]lose current  [T]ab" })
+
+vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show diagnostic [L]ine" })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Nvim tree toggle
+
+vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
+
+-- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
+-- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+-- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+-- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+-- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
