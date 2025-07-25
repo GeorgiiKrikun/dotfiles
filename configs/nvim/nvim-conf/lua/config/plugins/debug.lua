9f110fc75603ffdeccb40593325c560b4947fd29
Georgii <georgii.krikun@celantur.com>
2025-07-25 16:25:28 +0200

    Add debug config for CPP

--- configs/nvim/nvim-conf/lua/config/plugins/debug.lua
+++ configs/nvim/nvim-conf/lua/config/plugins/debug.lua
@@ -187,6 +187,54 @@ return {
         },
       }
 
+      dap.adapters.gdb = {
+          id = 'gdb',
+          type = 'executable',
+          command = 'gdb',
+          args = { '--quiet', '--interpreter=dap' },
+      }
+
+      dap.configurations.cpp = {
+        {
+            name = 'Run executable (GDB)',
+            type = 'gdb',
+            request = 'launch',
+            -- This requires special handling of 'run_last', see
+            -- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
+            program = function()
+                local path = vim.fn.input({
+                    prompt = 'Path to executable: ',
+                    default = vim.fn.getcwd() .. '/',
+                    completion = 'file',
+                })
+
+                return (path and path ~= '') and path or dap.ABORT
+            end,
+        },
+        {
+            name = 'Run executable with arguments (GDB)',
+            type = 'gdb',
+            request = 'launch',
+            -- This requires special handling of 'run_last', see
+            -- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
+            program = function()
+                local path = vim.fn.input({
+                    prompt = 'Path to executable: ',
+                    default = vim.fn.getcwd() .. '/',
+                    completion = 'file',
+                })
+
+                return (path and path ~= '') and path or dap.ABORT
+            end,
+            args = function()
+                local args_str = vim.fn.input({
+                    prompt = 'Arguments: ',
+                })
+                return vim.split(args_str, ' +')
+            end,
+        },
+      }
+
       dap.adapters.nlua = function(callback, config)
         callback({ type = 'server', host = config.host or "127.0.0.1", port = config.port or 8086 })
       end
