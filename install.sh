331ee6a6d6b4e8edbf3234bdbebf1e60b7aac80f
Georgii <georgii.krikun@celantur.com>
2025-09-11 15:44:06 +0000

    Some improvements

--- install.sh
+++ install.sh
@@ -6,34 +6,11 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 bash ${SCRIPT_DIR}/deps/bootstrap.sh 
 JUST=${HOME}/.cargo/bin/just
 
-if ! command -v zsh >/dev/null; then
-  ZSH_EXISTS=false
-else
-  ZSH_EXISTS=true
-fi
-
-if [ -d "${HOME}/.oh-my-zsh" ]; then
-  OH_MY_ZSH_EXISTS=true
-else
-  OH_MY_ZSH_EXISTS=false
-fi
-  
 cd ${SCRIPT_DIR}/deps
 
-if [ "$ZSH_EXISTS" = true ]; then
-  echo "Zsh exists, skipping Zsh install."
-else
-  echo "Zsh does not exist, we will install it."
-  ${JUST} install-zsh
-fi
-
-if [ "$OH_MY_ZSH_EXISTS" = true ]; then
-  echo "Oh My Zsh exists, skipping Oh My Zsh install."
-else
-  echo "Oh My Zsh does not exist, we will install it."
-  ${JUST} install-oh-my-zsh
-fi
-
+${JUST} install-zsh
+${JUST} install-oh-my-zsh
+  
 ${JUST} change-shell-zsh
 ${JUST} setup-cargo-zsh
 
@@ -60,4 +37,3 @@ ${HOME}/.cargo/bin/cargo install ripgrep
 ${HOME}/.cargo/bin/cargo install bottom 
 ${HOME}/.cargo/bin/cargo install fd-find
 echo "Utilities installed successfully."
-
