7d3369df294ba9f702d20d72c35b265cb40fd31e
Georgii <georgii.krikun@celantur.com>
2025-09-11 10:47:58 +0000

    Improve bootstrap

--- deps/bootstrap.sh
+++ deps/bootstrap.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
-set -ex
+set -e
 
-if ! command -v sudo >/dev/null; then
-  SUDO_EXISTS=false
-else
-  SUDO_EXISTS=true
-fi
-
-if [ "$SUDO_EXISTS" = true ]; then
-  echo "Sudo exists, proceeding with sudo commands."
-  sudo apt update && sudo apt install -y curl build-essential git
+if command -v sudo >/dev/null; then
+  echo "Sudo exists"
 else
   echo "Sudo does not exist, we will still need it to install nvim/node/zsh"
   exit 1
 fi
+  
+# sudo apt update && sudo apt install -y curl build-essential git
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-. $HOME/.cargo/env
-rustup default stable
-cargo install just
- 
-# For bash
-echo ". $HOME/.cargo/env" >> ${HOME}/.bashrc
+if command -v cargo >/dev/null; then
+  echo "Cargo exists"
+else
+  echo "Cargo does not exist, installing rust"
+  curl --proto '=https' --tlsv1.2 -ssf https://sh.rustup.rs | sh -s -- -y
+  . $home/.cargo/env
+  rustup default stable
+fi
+
+if command -v just >/dev/null; then
+  echo "Just exists"
+else 
+  cargo install just
+fi
+
+if cat ${HOME}/.bashrc | grep $HOME/.cargo/env >/dev/null; then 
+  echo "$HOME/.cargo/env already exists in ${HOME}/.bashrc"
+else
+  echo ". $HOME/.cargo/env" >> ${HOME}/.bashrc
+fi
