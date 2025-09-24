99f13365120406013075114f7a8ae0302344b694
Georgii Krikun <georgii.krikun@gmail.com>
2025-09-24 10:31:20 +0000

    Move everything to cargo binstall

--- deps/bootstrap.sh
+++ deps/bootstrap.sh
@@ -7,8 +7,33 @@ else
   echo "Sudo does not exist, we will still need it to install nvim/node/zsh"
   exit 1
 fi
-  
-# sudo apt update && sudo apt install -y curl build-essential git
+
+sudo apt update
+
+if command -v wget >/dev/null; then
+  echo "Wget exists"
+else 
+  sudo apt install -y wget
+fi
+
+if command -v curl >/dev/null; then
+  echo "Curl exists"
+else 
+  sudo apt install -y curl
+fi
+
+if command -v git >/dev/null; then
+  echo "Git exists"
+else 
+  sudo apt install -y git
+fi
+
+
+if command -v make >/dev/null; then
+  echo "Make exists"
+else 
+  sudo apt install -y make
+fi
 
 if command -v cargo >/dev/null; then
   echo "Cargo exists"
@@ -19,11 +44,20 @@ else
   rustup default stable
 fi
 
+
+if cargo --list | grep binstall ; then
+  echo "Cargo binstall exists"
+else
+  export BINSTALL_NO_CONFIRM=true
+  curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | sh -s -- -y
+fi
+
 if command -v just >/dev/null; then
   echo "Just exists"
 else 
-  cargo install just
+  cargo binstall just
 fi
+
 
 if cat ${HOME}/.bashrc | grep $HOME/.cargo/env >/dev/null; then 
   echo "$HOME/.cargo/env already exists in ${HOME}/.bashrc"
