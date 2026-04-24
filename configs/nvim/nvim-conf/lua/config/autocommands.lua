b9dfb1711755c66764c2c3c9f0a4635eab7dcc32
Georgii Krikun <georgii.krikun@gmail.com>
2026-04-24 15:53:36 +0000

    Fixes

--- configs/nvim/nvim-conf/lua/config/autocommands.lua
+++ configs/nvim/nvim-conf/lua/config/autocommands.lua
@@ -39,76 +39,10 @@ vim.api.nvim_create_autocmd("VimEnter", {
 	end,
 })
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
-	end,
-})
-
-
--- Preserve indentation on empty lines when saving
-vim.api.nvim_create_autocmd("BufWritePre", {
-	group = vim.api.nvim_create_augroup("indent_blank_lines", { clear = true }),
-	callback = function()
-		if not vim.bo.modifiable or vim.bo.buftype ~= "" then
-			return
-		end
-		local n_lines = vim.api.nvim_buf_line_count(0)
-		local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-		local changed = false
-
-		local ts_indent = nil
-		local has_ts, ts_module = pcall(require, "nvim-treesitter.indent")
-		if has_ts and ts_module.get_indent then
-			ts_indent = ts_module.get_indent
-		end
-
-		for i = 1, n_lines do
-			local line = lines[i]
-			if line:match("^%s*$") then
-				local indent = -1
-				if ts_indent then
-					local ok, res = pcall(ts_indent, i)
-					if ok then
-						indent = res
-					end
-				end
-
-				if indent == nil or indent < 0 then
-					local ok, res = pcall(vim.fn.indent, i)
-					if ok then
-						indent = res
-					end
-				end
-
-				if indent and type(indent) == "number" and indent > 0 then
-					local indent_str
-					if vim.bo.expandtab then
-						indent_str = string.rep(" ", indent)
-					else
-						local tabstop = vim.bo.tabstop or 8
-						indent_str = string.rep("\t", math.floor(indent / tabstop)) .. string.rep(" ", indent % tabstop)
-					end
-					if indent_str ~= line then
-						lines[i] = indent_str
-						changed = true
-					end
-				end
-			end
-		end
-
-		if changed then
-			local view = vim.fn.winsaveview()
-			vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
-			vim.fn.winrestview(view)
-		end
 	end,
 })
