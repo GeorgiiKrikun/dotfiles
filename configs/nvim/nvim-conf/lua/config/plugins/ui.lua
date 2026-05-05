7426836fa0a5d70480db1cf56142ff794d0cb7f8
Georgii Krikun <georgii.krikun@gmail.com>
2026-05-05 15:28:29 +0000

    Add vimmove plugin

--- configs/nvim/nvim-conf/lua/config/plugins/ui.lua
+++ configs/nvim/nvim-conf/lua/config/plugins/ui.lua
@@ -6,6 +6,33 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = { signs = false },
 	},
+    {
+        "letieu/jira.nvim",
+        opts = {
+            -- Your setup options...
+            jira = {
+                limit = 200,                                -- Global limit of tasks per view (default: 200)
+            },
+            projects = {
+                ["CEL"] = {
+                    story_point_field = "customfield_10016",      -- Custom field ID for story points
+                }
+            }
+        },
+    },
+    {
+        'MisanthropicBit/winmove.nvim',
+        keymaps = {
+            help = "?", -- Open floating window with help for the current mode
+            help_close = "q", -- Close the floating help window
+            quit = "q", -- Quit current mode
+            toggle_mode = "<tab>", -- Toggle between modes when in a mode
+        },
+        config = function(_, _)
+            vim.keymap.set("n", "<C-w>m", ":lua require(\"winmove\").start_mode(\"move\")<CR>", { desc = "Enter move mode" })
+            vim.keymap.set("n", "<C-w>r", ":lua require(\"winmove\").start_mode(\"resize\")<CR>", { desc = "Enter resize mode" })
+        end,
+    }
 	-- lazy.nvim
 	-- {
 	--   "folke/noice.nvim",
