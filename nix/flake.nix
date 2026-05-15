1dfc5193c6adfc132e127c1bedca098aa2573df8
Georgii <georgii.krikun@celantur.com>
2026-05-15 12:14:43 +0200

    Progress

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,16 +1,16 @@
 {
     description = "My core system dependencies";
 
+
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
         rust-overlay.url = "github:oxalica/rust-overlay";
-        neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     };
 
-    outputs = { self, nixpkgs, rust-overlay, nvim-nightly-overlay} :
+    outputs = { self, nixpkgs, rust-overlay} :
         let
             system = "x86_64-linux"; # Use "aarch64-darwin" if you ever move to an Apple Silicon Mac
-            overlays = [ (import rust-overlay) (import neovim-nightly-overlay) ];
+            overlays = [ (import rust-overlay) ];
             pkgs = import nixpkgs { inherit system overlays; };
             rustToolchain = pkgs.rust-bin.stable.latest.default;
         in {
