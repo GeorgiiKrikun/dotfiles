ba0c31d28d99806e67dc971b26fa51520ff67eba
Georgii <georgii.krikun@gmail.com>
2026-05-23 12:23:00 +0200

    Progress

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -6,7 +6,7 @@
 
 {
     imports =
-        [ # Include the results of the hardware scan.
+        [
             /etc/nixos/hardware-configuration.nix
         ];
 
@@ -99,10 +99,6 @@
         #media-session.enable = true;
     };
 
-    # Enable touchpad support (enabled default in most desktopManager).
-    # services.xserver.libinput.enable = true;
-
-    # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.georgii = {
         isNormalUser = true;
         description = "georgii";
@@ -113,7 +109,6 @@
         ];
         packages = with pkgs; [
             gparted
-            #  thunderbird
         ];
         shell = pkgs.zsh;
     };
@@ -154,36 +149,22 @@
         polkit_gnome
     ];
 
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
+    system.stateVersion = "25.11";
 
-    # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
-
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
-
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "25.11"; # Did you read the comment?
-
-    services.blueman.enable = true;  # optional GUI manager for bluetooth
+    services.blueman.enable = true;
     programs.steam = {
         enable = true;
         remotePlay.openFirewall = true;
     };
+
+    hardware.graphics.enable = true;
+    hardware.graphics.enable32Bit = true;
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = false;
+        open = true;
+    };
+    hardware.bluetooth.enable = true;
+    hardware.bluetooth.powerOnBoot = true;  # powers up on boot
 }
