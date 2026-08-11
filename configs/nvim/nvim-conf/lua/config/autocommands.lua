98ff257b2deb577171f9acd8b7cd3d84fe9171f5
Georgii <georgii.krikun@gmail.com>
2026-08-11 12:31:34 +0200

    Add Oil at startup

--- configs/nvim/nvim-conf/lua/config/autocommands.lua
+++ configs/nvim/nvim-conf/lua/config/autocommands.lua
@@ -26,6 +26,21 @@ vim.api.nvim_create_autocmd("TermOpen", {
 	end,
 })
 
+-- When neovim is opened without a file, open the current directory in oil.nvim
+vim.api.nvim_create_autocmd("VimEnter", {
+	once = true,
+	callback = function()
+		-- No file/dir arguments and the starting buffer is empty and unnamed
+		if vim.fn.argc() == 0 and vim.fn.line2byte(vim.fn.line("$")) == -1 and vim.api.nvim_buf_get_name(0) == "" then
+			-- Defer so oil's async directory scan runs after startup finishes,
+			-- otherwise the listing renders empty.
+			vim.schedule(function()
+				require("oil").open(vim.fn.getcwd())
+			end)
+		end
+	end,
+})
+
 -- Load custom commands from a file custom.lua
 vim.api.nvim_create_autocmd("VimEnter", {
 	once = true,
