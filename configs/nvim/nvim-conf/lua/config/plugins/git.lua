6ed5b37e3a999a48925c78f2bc70907c324b49d0
Georgii <georgii.krikun@gmail.com>
2026-08-11 13:05:50 +0200

    Add diffview

--- configs/nvim/nvim-conf/lua/config/plugins/git.lua
+++ configs/nvim/nvim-conf/lua/config/plugins/git.lua
@@ -11,6 +11,42 @@ return {
 			},
 		},
 	},
+	{ -- Side-by-side git diffs and file history using native diff mode
+		"sindrets/diffview.nvim",
+		cmd = {
+			"DiffviewOpen",
+			"DiffviewClose",
+			"DiffviewToggleFiles",
+			"DiffviewFocusFiles",
+			"DiffviewFileHistory",
+		},
+		keys = {
+			{ "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "[G]it [D]iff (working tree)" },
+			{ "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "[G]it diff [C]lose" },
+			{ "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "[G]it [H]istory (current file)" },
+			{ "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "[G]it [H]istory (whole repo)" },
+			-- Prompt for a revision/range, e.g. `main`, `HEAD~3`, or `main...HEAD`
+			{
+				"<leader>gb",
+				function()
+					vim.ui.input({ prompt = "Diff against (rev or range): " }, function(rev)
+						if rev and rev ~= "" then
+							vim.cmd("DiffviewOpen " .. rev)
+						end
+					end)
+				end,
+				desc = "[G]it diff against [B]ranch/rev",
+			},
+		},
+		opts = {
+			enhanced_diff_hl = true,
+			view = {
+				-- Native diff mode: ]c/[c, do/dp, :diffget/:diffput all work as usual
+				default = { layout = "diff2_horizontal" },
+				merge_tool = { layout = "diff3_horizontal" },
+			},
+		},
+	},
 	-- {
 	--	"kdheepak/lazygit.nvim",
 	--	lazy = true,
